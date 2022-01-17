5c2893302f5a29ef464d7483f2af2eb00725f983
Elias <91754540+eliosar@users.noreply.github.com>
2022-01-17 22:03:07 +0000

    datas are also now on the second Activity, Progressbar now innit

--- app/src/main/java/com/example/timer/StartedTimer.kt
+++ app/src/main/java/com/example/timer/StartedTimer.kt
@@ -1,20 +1,31 @@
 package com.example.timer
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ProgressBar
+import java.util.*
 
 class StartedTimer : AppCompatActivity() {
+
+    private var repeatMinutes: Int = 0
+    private var repeats: Int = 0
+    private var LOG_TAG: String? = ""
+    private var togoinpercent = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_started_timer)
 
-        val repeatMinutes = intent.getStringExtra(MainActivity::repeatMinutes.toString())
-        val repeats = intent.getStringExtra(MainActivity::repeats.toString())
-        val log_Tag = intent.getStringExtra(MainActivity::logTag.toString())
+        repeatMinutes = intent.getIntExtra(MainActivity::repeatMinutes.get(MainActivity()), repeatMinutes)
+        repeats = intent.getIntExtra(MainActivity::repeats.get(MainActivity()), repeats)
+        LOG_TAG = intent.getStringExtra(MainActivity::logTag.get(MainActivity()))
+    }
 
-        Log.d(log_Tag, "repeatMinutes: $repeatMinutes")
-        Log.d(log_Tag, "repeats: $repeats")
+    private fun progress(){
+        val pb = findViewById<ProgressBar>(R.id.timeprogressline)
+
+
     }
 }
