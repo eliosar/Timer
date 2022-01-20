ca7a0b90fb37b6d72ff44542a09b989e9d7b06d9
Elias <91754540+eliosar@users.noreply.github.com>
2022-01-20 21:20:59 +0000

    timer is now repeating itself

--- app/src/main/java/com/example/timer/StartedTimer.kt
+++ app/src/main/java/com/example/timer/StartedTimer.kt
@@ -3,27 +3,39 @@ package com.example.timer
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.TextView
 import java.util.*
-import kotlin.math.min
-import kotlin.math.roundToInt
-import kotlin.math.roundToLong
+import java.util.concurrent.Delayed
 
 class StartedTimer : AppCompatActivity() {
 
     private var repeatSeconds: Int = 0
     private var repeats: Int = 0
-    private var currentrepeats: Int = 0
     private var LOG_TAG: String? = ""
-    private var currentSeconds = 0
+    private var newtimer = true
     private var togo_in_percent: Float = 100f
-    private val timer = Timer()
+    private var currentrepeats = 1
+    private var timer: Timer? = Timer()
     private lateinit var pb: ProgressBar
     private lateinit var timerText: TextView
     private lateinit var repeatsText: TextView
+    private val timerthread = Thread {
+        while (currentrepeats <= repeats){
+            if(newtimer) {
+                Log.d(LOG_TAG, "$currentrepeats < $repeats")
+                newtimer = false
+                Handler(Looper.getMainLooper()).postDelayed({
+                    progress()
+                    repeatsText.text = "repeats: ${repeats - currentrepeats}"
+                }, 0)
+            }
+        }
+    }
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,19 +49,17 @@ class StartedTimer : AppCompatActivity() {
         pb = findViewById(R.id.timeprogressline)
         timerText = findViewById(R.id.timerText)
         repeatsText = findViewById(R.id.repeatsText)
-        repeatsText.text = "repeats: $repeats"
 
-        progress()
+        repeatsText.text = "repeats: $repeats"
+        timerthread.start()
     }
 
     private fun progress() {
-        currentSeconds = repeatSeconds
         Log.d(LOG_TAG, "new timer")
-
-        timer.schedule(object : TimerTask() {
+        var currentSeconds = repeatSeconds
+        timer?.scheduleAtFixedRate(object : TimerTask() {
             override fun run() {
                 togo_in_percent = (currentSeconds.toFloat() / repeatSeconds.toFloat()) * 100
-                Log.d(LOG_TAG, "${togo_in_percent.toInt()}% = $currentSeconds / $repeatSeconds")
 
                 pb.progress = togo_in_percent.toInt()
 
@@ -57,23 +67,24 @@ class StartedTimer : AppCompatActivity() {
 
                 if (currentSeconds == 0) {
                     Log.d(LOG_TAG, "timer finished")
+                    timer?.cancel()
+                    timer?.purge()
+                    timer = null
+                    timer = Timer()
 
-                    if (currentrepeats <= repeats) {
-                        currentrepeats++
-                        //repeatsText.text = "repeats: ${repeats - currentrepeats}" somehow issues
-                        progress()
-                    } else {
-                        pb.progress = 0
-                    }
-                    timer.cancel()
+                    currentrepeats++
+                    newtimer = true
                 }
                 currentSeconds--
             }
-        }, 0, 1000)
+        }, 0, 1000) // if program finished period = 1000
     }
 
     fun stopTimer(view: View){
-        timer.cancel()
+        timer?.cancel()
+        timer?.purge()
+        timer = null
+        timer = Timer()
         pb.progress = 0
         Log.d(LOG_TAG, "timer manual finished")
     }
